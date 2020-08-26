b3b8c540b3a7e2c37ab1b8adceef1e06b884fb7f
Janet Bah <janetm@pc40.com>
2020-08-26 01:31:48 -0500

    Login and Register Account created

---
+++ Areas/Identity/Data/Latin_DanceDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Latin_Dance.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Latin_Dance.Data
+{
+    public class Latin_DanceDbContext : IdentityDbContext<Latin_DanceUser>
+    {
+        public Latin_DanceDbContext(DbContextOptions<Latin_DanceDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Latin_Dance.Areas.Identity.Data;
+using Latin_Dance.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Latin_Dance.Areas.Identity.IdentityHostingStartup))]
+namespace Latin_Dance.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Latin_DanceDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Latin_DanceDbContextConnection")));
+
+                services.AddDefaultIdentity<Latin_DanceUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<Latin_DanceDbContext>();
+            });
+        }
+    }
+}
